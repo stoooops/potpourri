6b3c2a013f890e57909072812e8654220f3c454d
Cory Gabrielsen <corygabrielsen@gmail.com>
2021-08-22 01:49:08 -0700

    Expose pseudorandom helpers with seedrand

--- typescript/pseudorandom/pseudorandom.ts
+++ typescript/pseudorandom/pseudorandom.ts
@@ -1,13 +1,48 @@
-/**
- * @returns a pseudorandom number between 0 and 1
- */
-export function randFloat(): number {
-    return Math.random();
+import seedrandom from "seedrandom";
+
+// /**
+//  * @returns a pseudorandom number between 0 and 1
+//  */
+// export function randFloat(): number {
+//     return Math.random();
+// }
+
+const _seedrands = new Map();
+const _seedrandVals: Map<string, number[]> = new Map();
+
+interface _seedrandFloatInput {
+    seed: string;
+    index: number;
+}
+
+export function seedrandFloat(input: _seedrandFloatInput): number {
+    const seed: string = input.seed;
+    const index = input.index;
+
+    // get rng
+    let rng = _seedrands.get(seed);
+    if (rng === undefined) {
+        console.log(`Seed rng: ${seed}`);
+        rng = seedrandom(seed);
+        _seedrands.set(seed, rng);
+        _seedrandVals.set(seed, []);
+    }
+    let vals = _seedrandVals.get(seed) as number[];
+
+    // fill up vals
+    for (let i = vals.length; i <= index; i++) {
+        vals[i] = rng();
+    }
+    return vals[index];
+}
+
+interface _seedrandIntInput extends _seedrandFloatInput {
+    max: number;
 }
 
 /**
  * @returns a pseudorandom integer between 0 and given max
  */
-export function randInt(max: number): number {
-    return Math.floor(Math.random() * max);
+export function seedrandInt(seedrandIntInput: _seedrandIntInput): number {
+    return Math.floor(seedrandFloat(seedrandIntInput) * seedrandIntInput.max);
 }
