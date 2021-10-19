e814828a23c736ede383e74f80270a4a90fdc6e1
Cory Gabrielsen <corygabrielsen@gmail.com>
2021-10-18 20:17:02 -0700

    Add simple txlist call

--- python/ethereum/etherscan/client.py
+++ python/ethereum/etherscan/client.py
@@ -19,10 +19,29 @@ class EtherscanClient:
 
         return response_json
 
-    def get_account_balance(self, account: str) -> Decimal:
-        query_args = "&".join(["module=account", "action=balance", f"address={account}", "tag=latest"])
+    def get_account_balance(self, address: str) -> Decimal:
+        query_args = "&".join(["module=account", "action=balance", f"address={address}", "tag=latest"])
         url = f"{self._base_url}&{query_args}"
 
         response: Dict[str, str] = self.query(url)
         balance: str = response["result"]
         return Decimal(balance)
+
+    def get_txlist(self, address: str, start_block: int = 0, end_block: int = 99999999, page: int = 1, offset: int = 0):
+        query_args = "&".join(
+            [
+                "module=account",
+                "action=txlist",
+                f"address={address}",
+                f"startblock={start_block}",
+                f"endblock={end_block}",
+                f"page={page}",
+                f"offset={offset}",
+                "sort=asc",
+            ]
+        )
+        url = f"{self._base_url}&{query_args}"
+
+        response: Dict[str, str] = self.query(url)
+        result: str = response["result"]
+        return result
